cd3505cf5687cb1a5ce8567ea480fe97e8781df0
Peter <peter.gallacher@hotmail.co.uk>
2021-12-07 22:31:44 +0000

    update day 4

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -28,5 +28,40 @@ while line_number < len(i_a) - 1:
         line_number += 1
 
     scorecards.append(card)
-    
-print(scorecards)
+
+def mark_card(card, number):
+    for row in range(5):
+        for column in range(5):
+            if card[row][column] == number:
+                card[row][number] = None
+                return
+
+def get_card_total(card):
+    score = 0
+    for row in card:
+        for item in row:
+            if item is not None:
+                score += item
+    return score
+
+def find_winning_card(card):
+    count_row = 0
+    count_column = 0
+    for row in range(5):
+        for column in range(5):
+            if card[row][column] == None:
+                count_row += 1
+            if card[column][row] == None:
+                count_column += 1
+        if count_column == 5 or count_row == 5:
+            return True
+    return False
+
+
+for number in numbers_to_draw:
+    print('')
+    print(f"Number drawn: {number}")
+    for card in scorecards:
+        mark_card(card, number)
+        score = get_card_total(card)
+        winning_card = find_winning_card(card)
